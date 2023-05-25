dfe14de9ee65075cfa6ad5089d71114da2d7dfb1
Vijay Pathem <vijaypathem4@gmail.com>
2023-05-25 20:15:09 +0530

    polls app

--- polls/views.py
+++ polls/views.py
@@ -1,25 +1,34 @@
+from typing import Any
+from django.db import models
+from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
 from django.template import loader
 from django.urls import reverse
+from django.views.generic import (
+    ListView, DetailView
+)
+from django.utils import timezone
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+class IndexView(ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request, 'polls/detail.html', {'question':question})
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    question = get_object_or_404(Question,pk=question_id)
-    return render(request, 'polls/results.html', { 'question':question })
+class DetailView(DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+class ResultsView(DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question,pk=question_id)
